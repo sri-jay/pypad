4d62ec5f5115e2d4a45c67b5cc1c09b89054ab4e
Sriduth Jayhari <sriduth.jayhari@gmail.com>
2014-09-14 15:57:13 +0530

    Added {{ code }}

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def get_code(data):
 		print e
 
 	#return jsonify( {'CODE' : code[2],'EMAIL' : code[1],'COMMENTS' : code[3]} )
-	return render_template('view_code.html',comments=code[1],source=code[2],url=str(hashlib.sha224(str(random.random())).hexdigest()))
+	return render_template('view_code.html',comments=code[3],source=code[2],url=str(hashlib.sha224(str(random.random())).hexdigest()))
 
 if __name__ == "__main__":
 	app.run(debug=True)
